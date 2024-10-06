e7dab7d76c9a1f6f48a46a81e500ee173e45ce02
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-10-06 21:30:44 +0800

    Fix: new template

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Hero from "@/components/tailwind-components/hero";
+import Header from "@/components/header/Header";
+import HeroSection from "@/components/hero-section/HeroSection";
 
 const Home = () => {
-	return (
-		<main className="main">
-			<Hero></Hero>
-		</main>
-	);
+  return (
+    <main className="main">
+      <Header>My Header</Header>
+      <HeroSection />
+    </main>
+  );
 };
 
 export default Home;
---
+++ components/hero-section/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className="container max-w-screen-2xl mx-auto flex min-h-[90vh] ">
+      <div className="w-[50%]">hello</div>
+      <div className="w-[50%]">hello</div>
+    </div>
+  )
+}
+
+export default HeroSection
