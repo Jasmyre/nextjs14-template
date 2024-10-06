3243cc0b8167d59ca4c97e1bd0cbdef32df53afd
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-10-06 21:00:01 +0800

    tmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import HeroSection from '@/components/hero-section/HeroSection';
-import Header from "@/components/header/header"
+import HeroSection from "@/components/hero-section/HeroSection";
+import Header from "@/components/header/Header";
 
 const Home = () => {
-	return (
-		<main className="main">
-			<Header>My Header</Header>
-			<HeroSection></HeroSection>
-		</main>
-	);
+  return (
+    <main className="main">
+      <Header>My Header</Header>
+      <HeroSection></HeroSection>
+    </main>
+  );
 };
 
 export default Home;
--- components/hero-section/HeroSection.tsx
+++ components/hero-section/HeroSection.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from '../header/header';
+import React from "react";
+import Header from "../header/Header";
 
 const HeroSection = () => {
   return (
     <div>
-        <Header>My Header</Header>
+      <Header>My Header</Header>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
