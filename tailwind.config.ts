efb96ab871ab667ccb0aff4b5bc2cdc27efba20f
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-10-19 14:35:29 +0800

    Fix; small errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,29 +89,11 @@ const config: Config = {
   					zIndex: '1'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
   			'float-up': 'float-up 0.5s ease',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		},
   		fontFamily: {
   			sans: ["Inter var", ...defaultTheme.fontFamily.sans]
